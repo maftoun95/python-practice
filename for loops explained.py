d3225eaca27379f9f73189d81f45963930ae22d8
maftoun95 <maftoun95@gmail.com>
2017-01-14 23:03:45 -0800

    added for loop explainer

---
+++ for loops explained.py
@@ -0,0 +1,23 @@
+dictionary = {'key1':'val1','key2':'val2','key3':'val3','key4':'val4'}
+print 'dictionary ish'
+for thingy in dictionary:
+    print 'variable:'
+    print thingy
+    print 'dictionary[thingy]'
+    print dictionary[thingy]
+    print '*'*25
+print 'NOTE THAT WITH A DICTIONARY, THE VARIABLE IS THE KEY'
+listt = [11,22,33,44]
+print 'list ish'
+for thingy in listt:
+    print 'variable:'
+    print thingy
+    print '*'*25
+print 'NOTE THAT WITH A LIST, THE VARIABLE IS THE VALUE'
+tulip = (11,22,33,44)
+print 'tuple ish'
+for thingy in tulip:
+    print 'variable:'
+    print thingy
+    print '*'*25
+print 'NOTE THAT WITH A DICTIONARY, THE VARIABLE IS THE VALUE'
